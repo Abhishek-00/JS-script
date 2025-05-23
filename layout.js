ee9b3bfd9640468014921fd1112699c7b0570a47
Abhishek Kumar <abhishek02102003@gmail.com>
2025-05-23 19:41:49 +0530

    change-1

--- layout.js
+++ layout.js
@@ -1,1 +1,4 @@
-console.log('hello from layout.js');
+const multicolumn = document.querySelector('#multicolumn_1747938980932');
+const imageContainer = document.querySelector('.multicolumn-v1__container-wrapper');
+console.log('hello from layout.js', imageContainer);
+imageContainer.style.gridTemplateColumns = '1fr 1fr 1fr';
